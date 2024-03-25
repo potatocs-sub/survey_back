2161c182d17b05fb10828023e1b72f419c3c55c5
limhokyun <lim@nsmarts.co.kr>
2024-03-25 10:21:10 +0900

    first commit

---
+++ database/mongoDB.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+
+const mongoApp = {};
+mongoApp.appSetObjectId = function (app) {
+    app.set('objectId', mongoose.Types.ObjectId);
+    console.log('complete to set mongoose ObjectId');
+}
+
+main().catch((err) => console.log(err));
+
+async function main() {
+    await mongoose.connect(process.env.MONGODB_URI).then(() => {
+        createSchema();
+        console.log('Database Connected')
+    })
+}
+
+/** 스키마 정의 */
+function createSchema() {
+    const dbModels = {};
+    dbModels.Survey = require('../schemas/survey_schema');
+
+    global.DB_MODELS = dbModels;
+}
+
+module.exports = mongoApp;
---
+++ server.js
@@ -0,0 +1,40 @@
+// es5 
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const { Server } = require('socket.io');
+
+// production, development 
+if (process.env.NODE_ENV.trim() === 'production') {
+    require('dotenv').config({ path: path.join(__dirname, '/env/prod.env') })
+} else if (process.env.NODE_ENV.trim() === 'development') {
+    require('dotenv').config({ path: path.join(__dirname, '/env/dev.env') })
+}
+
+const allowedOrigins = [
+    "http://localhost:4200"
+]
+
+// origin: cors 정책
+// allowedHeaders: 허용할 헤더 지정
+// credentials: 자격 증명을 포함하여 요청을 허용할지 여부를 결정 - 쿠키, http 인증 등...
+app.use(cors({ origin: allowedOrigins, allowedHeaders: ["Content-Type", "Authorization"], credentials: true }));
+app.use(express.urlencoded({ limit: "400mb", extended: true }));
+app.use(express.json({ limit: "400mb" }));
+app.use(cookieParser());
+
+const mongoApp = require('./database/mongoDB');
+
+const server = http.createServer(app).listen(port, '0.0.0.0', () => {
+    cconsole.log(`app listening on port ${port}`);
+    mongoApp.appSetObjectId(app);
+})
+
+
+
+
+// express 정의
+const app = express();
+const port = process.env.PORT || 3000;
