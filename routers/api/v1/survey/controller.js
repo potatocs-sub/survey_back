e5718bda05c9e2314bffdcda56791efa4d115c36
limhokyun <lim@nsmarts.co.kr>
2024-03-25 13:52:32 +0900

    [feat] 설문지 리스트, 특정 설문지 요청 api 작성

--- routers/api/v1/survey/controller.js
+++ routers/api/v1/survey/controller.js
@@ -1,3 +1,8 @@
+/**
+ * @desciprtion 설문지 등록
+ * @param {*} req 
+ * @param {*} res 
+ */
 exports.addSurvey = async (req, res) => {
     console.log(`
 --------------------------------------------------
@@ -18,6 +23,58 @@ exports.addSurvey = async (req, res) => {
             message: "An error occured while adding survey"
         })
     }
+}
+/**
+ * @description 설문지 리스트 요청
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.getSurveys = async (req, res) => {
+    console.log(`
+    --------------------------------------------------
+      User : 
+      API  : getSurveys
+      router.get('/survey', surveyController.getSurveys);
+    --------------------------------------------------
+        `)
 
+    const dbModels = global.DB_MODELS;
+    try {
+        const surveys = await dbModels.Survey.find();
+        console.log(surveys);
+        res.status(200).json(surveys);
+    } catch (err) {
+        console.log("[ ERROR ]", err);
+        res.status(500).send({
+            message: "An error occured while getting survey"
+        })
+    }
+}
 
+/**
+ * @description 설문지 정보 요청
+ * @param {*} req 
+ * @param {*} res 
+ */
+exports.getSurvey = async (req, res) => {
+    console.log(`
+    --------------------------------------------------
+      User : 
+      API  : getSurvey
+      router.get('/survey/:_id', surveyController.getSurvey);
+    --------------------------------------------------
+        `)
+
+    const dbModels = global.DB_MODELS;
+    const _id = req.query._id;
+    try {
+        const survey = await dbModels.Survey.findOne({ _id })
+
+        res.status(200).json(survey);
+    } catch (err) {
+        console.log("[ ERROR ]", err);
+        res.status(500).send({
+            message: "An error occured while getting survey"
+        })
+    }
 }
--- routers/api/v1/survey/index.js
+++ routers/api/v1/survey/index.js
@@ -1,6 +1,13 @@
 const router = require('express').Router();
 const surveyController = require('./controller');
 
+// 설문지 등록
 router.post('/add', surveyController.addSurvey);
+// 설문지 리스트 요청
+router.get('/', surveyController.getSurveys);
+// 설문지 요청
+router.get('/:_id', surveyController.getSurvey);
+// 설문지 수정
+// router.patch('/:_id', )
 
 module.exports = router;
